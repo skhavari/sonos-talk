35314d02efba709deff9a529f0efe6b9517dba0b
Sam Khavari <skhavari@cs.stanford.edu>
2014-06-26 23:34:37 -0700

    Server detects sonos zone players on startup

--- server.go
+++ server.go
@@ -1,29 +1,85 @@
 package main
 
+//
+//  No build for this yet.  To run:
+//
+//  run> go get github.com/ianr0bkny/go-sonos
+//  run> go run server.go
+//
+//
+
 import (
+	"github.com/ianr0bkny/go-sonos"
+	"github.com/ianr0bkny/go-sonos/ssdp"
 	"log"
 	"net/http"
 )
 
+const (
+	// for sonos discovery
+	// TODO: compute these automatically or use a config file
+	TEST_DISCOVER_PORT = "13111"
+	TEST_EVENTING_PORT = "13333"
+	TEST_NETWORK       = "en0"
+
+	// HTTPS server
+	SSL_CERT_FILE = "./keys/cert.pem"
+	SSL_KEY_FILE  = "./keys/key.pem"
+	SSL_PORT      = ":8443"
+)
+
+//
+// A Handler that wraps a handler so all calls to the handler are logged
+//
 func Log(handler http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
 }
 
+//
+// Search for Sonos ZonePlayers
+//
+func DetectZP() (zonePlayers ssdp.DeviceMap, err error) {
+
+	mgr, err := sonos.Discover(TEST_NETWORK, TEST_DISCOVER_PORT)
+	if nil != err {
+		return
+	}
+
+	zonePlayers = make(ssdp.DeviceMap)
+	for uuid, device := range mgr.Devices() {
+		if device.Product() == "Sonos" && device.Name() == "ZonePlayer" {
+			zonePlayers[uuid] = device
+		}
+	}
+	return
+}
+
+//
+// Main
+//
 func main() {
 
-	log.SetFlags(log.Ltime | log.Lshortfile)
+	// light logging
+	log.SetFlags(log.Ltime)
 
-	cert := "./keys/cert.pem"
-	key := "./keys/key.pem"
-	port := ":8443"
+	log.Println("Detecting Sonos ZonePlayers")
+	zonePlayers, err := DetectZP()
+	if err != nil {
+		log.Println("Error detecting Sonos ZonePlayers", err)
+		return
+	}
+	log.Println("Detected", len(zonePlayers), "Sonos ZonePlayers")
+
+	// serve files in ./webroot/
 	handler := Log(http.FileServer(http.Dir("./webroot/")))
 
-	log.Println("Listening securely on port ", port)
+	// info msg
+	log.Println("Listening securely on port ", SSL_PORT)
 
-	// without HTTPS chrome prompts every time for microphone access
-	// with HTTPS it remembers the users choice
-	log.Fatal(http.ListenAndServeTLS(port, cert, key, handler))
+	// listen kinda securely
+	log.Fatal(http.ListenAndServeTLS(SSL_PORT, SSL_CERT_FILE, SSL_KEY_FILE, handler))
 }
